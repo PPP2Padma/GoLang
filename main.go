39bca002bf9ed7d519993d72ef35929d140e9892
Priya <Padma.2586@gmail.com>
2025-06-18 06:11:01 +0000

    test

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Hello from GitHub Codesaces!")
+    fmt.Fprintln(w, "Hello from GitHub codesapces!")
 }
 
 func main() {
